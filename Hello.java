47897f4c6870c17070d51d6a12ee6febac985a5c
niveditha <nivibe96@gmail.com>
2017-07-06 17:24:24 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.io.*;
+//import java.util.*;
+
+public class Hello
+{
+    public static void main(String args[])throws IOException,NullPointerException
+    {    //String s;                                      r
+        ///BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        //String s = br.readLine();
+        for(int i=0;i<5;i++)
+        {
+          
+          System.out.println("Hello");
+          }
+          }
+          }
